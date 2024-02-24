9563246e8d16e12fd601bf662a67c8dd0f592c3f
5885Wiredcats <team@wiredcats5885.ca>
2024-02-24 15:45:24 -0500

    Converted back to falcon???
    bruh

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
@@ -23,10 +24,9 @@ public class WristSubsystem extends WCStaticSubsystem {
   // Buffer is value slightly above 0 to ensure doesn't smack
   private final double buffer = 0.0;
 
-  private CANSparkMax m_wrist;
+  private TalonFX m_wrist;
   private PIDController m_PidController;
   private double m_setPoint;
-  private SparkAbsoluteEncoder m_absoluteEncoder;
 
   @Override
   protected double getBaseSpeed() {
@@ -35,10 +35,9 @@ public class WristSubsystem extends WCStaticSubsystem {
 
   @Override
   protected List<MotorController> initMotors() {
-    m_wrist = new CANSparkMax(Constants.kWrist, MotorType.kBrushless);
+    m_wrist = new TalonFX(Constants.kWrist);
     m_PidController = new PIDController(1.0, 0.0, 0.0);
     m_PidController.enableContinuousInput(0, 2 * Math.PI);
-    m_absoluteEncoder = m_wrist.getAbsoluteEncoder(Type.kDutyCycle);
     // m_arm.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 1, 0);
     // TalonFXConfiguration config = new TalonFXConfiguration();
     return List.of(m_wrist);
@@ -47,14 +46,14 @@ public class WristSubsystem extends WCStaticSubsystem {
   // LOWER LIMIT ISNT ZERO IT WILL START IN AN IN BETWEEN
   @Override
   public void periodic() {
-    // System.out.println("Wrist Position" + m_absoluteEncoder.getPosition());
+    // System.out.println("Wrist Position" + m_wrist.getPosition().getValueAsDouble());
     if (subsystemAction == SubsystemAction.UP && isAtUpperLimit()) {
       forwardMotors();
     } else if (subsystemAction == SubsystemAction.DOWN && isAtLowerLimit()) {
       reverseMotors();
     } else if (subsystemAction == SubsystemAction.POS) {
       double measurement =
-          RobotSystem.isReal() ? m_absoluteEncoder.getPosition() * 2 * Math.PI : positionSim;
+          RobotSystem.isReal() ? m_wrist.getPosition().getValueAsDouble() * 2 * Math.PI : positionSim;
       double setpoint = m_setPoint * 2 * Math.PI;
       m_wrist.setVoltage(m_PidController.calculate(measurement, setpoint));
       if (measurement == setpoint) {
@@ -63,22 +62,22 @@ public class WristSubsystem extends WCStaticSubsystem {
     } else {
       stopMotors();
     }
-    positionSim += m_wrist.getAppliedOutput() * 0.02;
-    Logger.recordOutput("wristOutput", m_wrist.getAppliedOutput());
+    positionSim += m_wrist.getMotorVoltage().getValueAsDouble() * 0.02;
+    Logger.recordOutput("wristOutput", m_wrist.getMotorVoltage().getValueAsDouble());
     Logger.recordOutput(
-        "wristPosition", RobotSystem.isReal() ? m_absoluteEncoder.getPosition() : positionSim);
+        "wristPosition", RobotSystem.isReal() ? m_wrist.getPosition().getValueAsDouble() : positionSim);
     Logger.recordOutput("wristUP", subsystemAction == SubsystemAction.UP);
     Logger.recordOutput("wristDown", subsystemAction == SubsystemAction.DOWN);
     Logger.recordOutput("wristPos", subsystemAction == SubsystemAction.POS);
   }
 
   private boolean isAtUpperLimit() {
-    double wristPosition = RobotSystem.isReal() ? m_absoluteEncoder.getPosition() : positionSim;
+    double wristPosition = RobotSystem.isReal() ? m_wrist.getPosition().getValueAsDouble() : positionSim;
     return wristPosition < Constants.kWristEncoderMax + buffer;
   }
 
   private boolean isAtLowerLimit() {
-    double wristPosition = RobotSystem.isReal() ? m_absoluteEncoder.getPosition() : positionSim;
+    double wristPosition = RobotSystem.isReal() ? m_wrist.getPosition().getValueAsDouble() : positionSim;
     return wristPosition > Constants.kWristEncoderMin - buffer;
   }
 
