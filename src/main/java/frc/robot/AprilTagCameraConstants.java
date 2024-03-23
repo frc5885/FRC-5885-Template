33d164dd6399507724f84e50a9b1d9fc44f8700f
5885Wiredcats <team@wiredcats5885.ca>
2024-03-22 21:52:31 -0400

    Colour is changable by only hardcode
    bruh

--- src/main/java/frc/robot/AprilTagCameraConstants.java
+++ src/main/java/frc/robot/AprilTagCameraConstants.java
@@ -17,8 +17,10 @@ public class AprilTagCameraConstants {
   public class Shooter {
     public static final String kCameraName = "USB_Camera";
     // Higher offset = further away
-    // public static final double kCameraPositionX = Units.inchesToMeters(11.56 + 0.39 + 14.0);
-    public static final double kCameraPositionX = Units.inchesToMeters(11.56 + 0.39 + 12.0);
+    // Blue
+    public static final double kCameraPositionX = Units.inchesToMeters(11.56 + 0.39 + 14.0);
+    // Red
+    // public static final double kCameraPositionX = Units.inchesToMeters(11.56 + 0.39 + 12.0);
     public static final double kCameraPositonY = 0.0;
     public static final double kCameraPositionZ = Units.inchesToMeters(13.876 + 1.9);
     public static final double kCameraRoll = 0.0;
