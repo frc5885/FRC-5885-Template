9fe0adcee5d371e7101dd4f1d810b5cfc2d2fb10
Nick <13785390+quizque@users.noreply.github.com>
2024-03-25 20:44:24 -0400

    temp code reverse to tune pid

--- src/main/java/frc/robot/commands/AimShooterCommand.java
+++ src/main/java/frc/robot/commands/AimShooterCommand.java
@@ -59,17 +59,17 @@ public class AimShooterCommand extends Command {
   @Override
   public void execute() {
     if (m_beambreak.isBroken() && m_armSubsystem.isArmDown()) {
-      m_robot.setSwerveAction(SwerveAction.AIMBOTTING);
+      // m_robot.setSwerveAction(SwerveAction.AIMBOTTING);
       double distanceToTarget =
           m_photonVision.getDistanceToTarget(
               m_swervePoseEstimator.getPose(), m_photonVision.getTargetID());
       double wristAngle = WristAngleUtil.getAngle(distanceToTarget);
       // double wristAngle = SmartDashboard.getNumber("SHOOTPOINT", Constants.kWristAmp);
-      if (distanceToTarget >= 3.1) {
-        m_shooterSubsystem.spinFastFar();
-      } else {
-        m_shooterSubsystem.spinFastClose();
-      }
+      // if (distanceToTarget >= 3.1) {
+      //   m_shooterSubsystem.spinFastFar();
+      // } else {
+      //   m_shooterSubsystem.spinFastClose();
+      // }
 
       if (distanceToTarget >= 5.0) {
         m_WristSubsystem.pos(Constants.kWristPass);
